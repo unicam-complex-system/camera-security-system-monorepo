cc9fc451cd1aecf498358261b19294339cca8e75
Glydric <git@miglio.dev>
2023-11-25 11:49:06 +0100

    added tests for jwt token

--- server/src/auth/auth.guard.spec.ts
+++ server/src/auth/auth.guard.spec.ts
@@ -2,7 +2,25 @@ import { AuthGuard } from "./auth.guard";
 import { JwtService } from "@nestjs/jwt";
 
 describe("AuthGuard", () => {
+  const payload = "complexUserNamePayload";
+  let auth: AuthGuard;
+  let jwtToken: string;
+
+  beforeAll(async () => {
+    const service = new JwtService({
+      secret: "TestKey",
+    });
+    auth = new AuthGuard(service);
+    jwtToken = await service.signAsync(payload);
+  });
+
   it("should be defined", () => {
-    expect(new AuthGuard(new JwtService())).toBeDefined();
+    expect(auth).toBeDefined();
+    expect(jwtToken).toBeDefined();
+  });
+
+  it("Get Payload from token", async () => {
+    const user = await auth.checkUser(jwtToken);
+    expect(user).toBe(payload);
   });
 });
--- server/src/auth/auth.guard.ts
+++ server/src/auth/auth.guard.ts
@@ -1,6 +1,7 @@
 import {
   CanActivate,
   ExecutionContext,
+  ForbiddenException,
   HttpException,
   HttpStatus,
   Injectable,
@@ -10,30 +11,38 @@ import { Request } from "express";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+
   constructor(private jwtService: JwtService) {}
 
-  //The canActivate method is called before the route handler, it saves the user object to the request["user"] object
+  // The canActivate method is called before the route handler, it saves the user object to the request["user"] object
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
-    if (!token) {
-      throw new HttpException("Unauthorized", HttpStatus.UNAUTHORIZED);
-    }
+
     try {
-      // We don't check if the user exists in db because the token is generated from us and only if the user exists
-      // Maybe we can check if the user exists in db and if not throw an error, in the case the user have been deleted
-      // But for now we don't delete users and users do not have defined authorization levels
-      request["user"] = await this.jwtService.verifyAsync(token, {
-        secret: process.env.JWT_SECRET,
-      });
-    } catch {
-      throw new HttpException("Unauthorized", HttpStatus.UNAUTHORIZED);
+      request["user"] = await this.checkUser(token);
+    } catch (e) {
+      throw new HttpException(e.message, HttpStatus.UNAUTHORIZED);
     }
     return true;
   }
 
-  private extractTokenFromHeader(request: Request): string | undefined {
+  // We don't check if the user exists in db because the token is generated from us and only if the user exists
+  // Maybe we can check if the user exists in db and if not throw an error, in the case the user have been deleted
+  // But for now we don't delete users and users do not have defined authorization levels
+  checkUser(token: string) {
+    return this.jwtService.verifyAsync(token, {
+      secret: process.env.JWT_SECRET,
+    });
+  }
+
+  private extractTokenFromHeader(request: Request): string {
     const [type, token] = request.headers.authorization?.split(" ") ?? [];
-    return type === "Bearer" ? token : undefined;
+
+    if (type != "Bearer") {
+      throw new ForbiddenException("No token provided");
+    }
+
+    return token;
   }
 }
