79f9a35fff9e48647390b9c77d5adcae56e7ba2e
Glydric <git@miglio.dev>
2023-11-30 15:58:17 +0100

    merging

--- update-submodule.bat
+++
@@ -1,5 +0,0 @@
-git submodule update --init --recursive
-git pull --recurse-submodules 
-git add .
-git commit -am "automatic submodule update" 
-git push
--- update-submodule.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-git submodule update --init --recursive
-git pull --recurse-submodules 
-git add .
-git commit -am "automatic submodule update" 
-git push
