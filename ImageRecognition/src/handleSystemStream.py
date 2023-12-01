3ed3fc19f4aeaff708983eb7026a7bacd8e8ed03
Glydric <git@miglio.dev>
2023-12-01 14:29:00 +0100

    improvements

--- ImageRecognition/src/handleSystemStream.py
+++ ImageRecognition/src/handleSystemStream.py
@@ -1,14 +1,11 @@
-import os
-import subprocess
-
 import cv2
 import ffmpeg
 import numpy as np
 from ping3 import ping
 
-# The function uses outFormat where the char "%d" is the frame count, also remember to define all path
-def handle_status(ip: str, down: bool):
-    if down:
+
+def handle_status(ip: str, is_available: bool):
+    if is_available:
         print(f"Failed connection with ip: {ip}")
     else:
         print(f"Connected with ip: {ip}")
@@ -16,13 +13,16 @@ def handle_status(ip: str, down: bool):
     # TODO save log
 
 
-opts = {"loglevel": "quiet", "r": "30", "f": "avfoundation"}
+opts = {
+    "loglevel": "quiet",  # "r": "30", "f": "avfoundation"
+    "headers": 'Authorization: Basic YWRtaW46'
+}
 
 
-def handle_connection(ip: str, out_format: str):
+def handle_connection(ip: str, debug: bool):
     # Run the command using subprocess
     cmd = (ffmpeg
-           .input("0:", **opts)
+           .input(f"http://{ip}/livestream/11", **opts)
            .output("pipe:", format="rawvideo", pix_fmt="bgr24")
            .run_async(pipe_stdout=True)
            )
@@ -32,40 +32,16 @@ def handle_connection(ip: str, out_format: str):
             break
         frame = np.frombuffer(raw_frame, np.uint8).reshape((1080, 1920, 3))
 
-        cv2.imshow("VideoFrame", frame)
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-    try:
-        print("Frames extracted successfully.")
-    except subprocess.CalledProcessError as e:
-        print(e.returncode)
-        print(f"Error: {e}")
+        if debug:
+            cv2.imshow(f"VideoFrame{ip}", frame)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
 
 
-def obtain_frames(ip: str, out_format: str, debug: bool):
-    if not dir_check(out_format, not debug):
-        raise Exception("Path not usable")
-
+def obtain_frames(ip: str, debug: bool):
     while True:
-        if ping(ip) is None:
-            handle_status(ip, True)
-        else:
-            handle_status(ip, False)
-            handle_connection(ip, out_format)
+        available = ping(ip) is not None
+        handle_status(ip, available)
 
-
-def dir_check(directory: str, force: bool):
-    out_path = os.path.dirname(directory)
-
-    if not os.path.exists(out_path):
-        if force:
-            os.makedirs(out_path)
-            return True
-
-        choice = input(f"Directory '{out_path}' not exists. You want to create it? (Yes/No): ")
-        if choice.lower() == 'no':
-            print("Operazione annullata.")
-            return False
-
-        os.makedirs(out_path)
-    return True
+        if available:
+            handle_connection(ip, debug)
